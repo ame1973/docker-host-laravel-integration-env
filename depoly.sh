48dddf125f10a9b47efbe41bd3a37aa27cd4f815
Ubuntu <ubuntu@ip-172-31-26-88.ec2.internal>
2021-10-29 13:38:34 +0000

    update deploy.sh

--- depoly.sh
+++ depoly.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+read -p 'Project Name: ' projectName
+read -p 'Project Domain: ' projectDomain
+
 cp docker-compose.example docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_NAME/$1/g" docker-compose.yml
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
 
 cp ./src/.env.example ./src/.env
 sed -i "s/DB_HOST=.*/DB_HOST=mysql/g" ./src/.env
 sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=password/g" ./src/.env
-sed -i "s/APP_NAME=.*/APP_NAME=$1/g" ./src/.env
+sed -i "s/APP_NAME=.*/APP_NAME=$projectName/g" ./src/.env
 sed -i "s/APP_ENV=.*/APP_ENV=production/g" ./src/.env
-sed -i "s/APP_URL=.*/APP_URL=https:\/\/$2/g" ./src/.env
+sed -i "s/APP_URL=.*/APP_URL=https:\/\/$projectDomain/g" ./src/.env
