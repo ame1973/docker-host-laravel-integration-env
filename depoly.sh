30df3b02686eb2d924fd080f1f58b791c078050f
HuanXiang <qq418610675@gmail.com>
2022-03-20 08:53:06 +0800

    update

--- depoly.sh
+++ depoly.sh
@@ -16,3 +16,6 @@ sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=password/g" ./src/.env
 sed -i "s/APP_NAME=.*/APP_NAME=$projectName/g" ./src/.env
 sed -i "s/APP_ENV=.*/APP_ENV=production/g" ./src/.env
 sed -i "s/APP_URL=.*/APP_URL=https:\/\/$projectDomain/g" ./src/.env
+
+cp ./backup/backup_db.sh.example backup_db.sh
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" backup_db.sh
