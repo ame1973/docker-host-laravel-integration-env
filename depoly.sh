098c93810b4aef949ef93194e861a68c0975969b
HuanXiang <qq418610675@gmail.com>
2022-03-20 09:03:34 +0800

    update

--- depoly.sh
+++ depoly.sh
@@ -8,7 +8,21 @@ cp docker-compose.example docker-compose.yml
 sed -i "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
 sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
+DEFAULT="y"
+read -e -p "Enable redis? [Y/n]:" eRedis
+eRedis="${eRedis:-${DEFAULT}}"
+
+if [ "${eRedis}" != "y" ] ; then
+	sed -i "s/#redis//g" docker-compose.yml
+fi
+
+read -e -p "Enable meilisearch? [Y/n]:" eMeilisearch
+eMeilisearch="${eRedis:-${DEFAULT}}"
+
+
+if [ "${eMeilisearch}" != "y" ] ; then
+	sed -i "s/#meilisearch//g" docker-compose.yml
+fi
 
 cp ./src/.env.example ./src/.env
 sed -i "s/DB_HOST=.*/DB_HOST=mysql/g" ./src/.env
